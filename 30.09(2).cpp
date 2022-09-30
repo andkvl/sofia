bba8e5801cc8b9a3bd1ee297a6dbe8ec270a290d
andkvl <84006447+andkvl@users.noreply.github.com>
2022-09-30 07:17:00 +0300

    Add files via upload

---
+++ 30.09(2).cpp
@@ -0,0 +1,17 @@
+
+{
+int kr,kt;
+double cr,ct,s;
+cout<<"Skolko ruchek? ";
+cin >>kr;
+cout<<"Cena? ";
+cin >>cr;
+cout<<"Skolko tetradei? ";
+cin >>kt;
+cout<<"Cena? ";
+cin >>ct;
+s=cr*kr+ct*kt;
+cout<<"Itogo: "<<s<<'$'<<endl;
+cout<<"Spasibo za pokypky!";
+return 0;
+}
